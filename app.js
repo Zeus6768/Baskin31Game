4eeb2ce0184d24df595aa7e91d8959dfca65e3dd
Zeus6768 <akaps6768@gmail.com>
2022-06-07 21:54:17 +0900

    Game logic : number emitting and receiving

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express"); //express 모듈 불러오기
-const socket = require('socket.io') //soket.io 모듈 불러오기
+const socket = require('socket.io'); //soket.io 모듈 불러오기
 const http = require('http'); //node.js 기본 내장 모듈 불러오기
 const app = express(); //express 객체 생성
 const server = http.createServer(app) // express http server 생성
 const io = socket(server) // 생성된 서버를 socket.io에 바인딩
-const port = 15000; //접속포트번호zz
+const port = 15000; //접속포트번호
 
-app.use(express.static("page")) //page dir 이용
+app.use(express.static("page"))
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/page');
 
+///////////
+// 라우팅 //
+///////////
 app.get('/', (_, res) => {
 	res.render('start.html', { userCount: getUserCount() });
 })
@@ -42,6 +45,10 @@ app.get('/game', (req, res) => {
 	res.render('game.html', { username: username });
 })
 
+
+///////////
+// 웹소켓 //
+///////////
 const MAXUSER = 5;
 let usersInReady = new Array();
 let usersInGame = new Array();
@@ -52,14 +59,13 @@ io.on('connection', socket => {
 	const req = socket.request;
 	const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress.substr(7);
 
-	// 새로운 접속자가 이름을 입력할 시 다른 소켓에게 알려줌
 	socket.on('userInReady', data => {
 		if (getUserCount() < MAXUSER) {
 			const user = {
 				id: socket.id,
 				ip: ip,
 				name: data.username,
-				status: 'ready'
+				inGame: false
 			}
 			usersInReady.push(user);
 			console.log(data.username, '님이 접속했습니다. 대기중 유저 수 :', getUserCount());
@@ -73,27 +79,29 @@ io.on('connection', socket => {
 				id: socket.id,
 				ip: ip,
 				name: username,
-				status: 'inGame'
+				inGame: true
 			}
 			usersInGame.push(user);
 			console.log(username, '님이 게임을 시작했습니다. 게임중 유저 수 :', getUserCount());	
 		}
-		
 		io.emit('usersInServer', usersInServer());
 		
 		if (usersInGame.length === MAXUSER) {
 			console.log('게임이 시작되었습니다');
-			data = { message: '게임이 시작되었습니다.', gameActive: true }
+			const data = {
+				message: '게임이 시작되었습니다.',
+				gameActive: true,
+				firstUser: usersInGame[0].name
+			}
 			io.emit('gameStart', data);
 		} else {
 			console.log('유저가 모두 들어오지 않아 게임을 진행할 수 없습니다.');
-			data = { message: '유저가 모두 들어오지 않아 게임을 진행할 수 없습니다.', gameActive: false }
-			io.emit('gameStart', data);
+			io.emit('gameStart', { message: '유저가 모두 들어오지 않아 게임을 진행할 수 없습니다.', gameActive: false });
 		}
 	})
 
-	socket.on('submitNumber', data => {
-		io.emit('broadcastNumber', data)
+	socket.on('submitNumber', number => {
+		io.emit('broadcastNumber', number)
 	})
 
 	socket.on('disconnect', () => {
@@ -105,11 +113,12 @@ io.on('connection', socket => {
 		} else if (ig !== -1) {
 			console.log(usersInGame[ig].name, '님이 게임에서 나갔습니다.' );
 			usersInGame = usersInGame.filter(user => user.id !== socket.id);
+			io.emit('gameStart', { message: '유저가 모두 들어오지 않아 게임을 진행할 수 없습니다.', gameActive: false });
 		} else {
 			console.log(ip, socket.id, '올바르지 않은 접속입니다.')
 		}
-		io.emit('usersInServer', usersInServer());
 		console.log('users in server:', usersInServer());
+		io.emit('usersInServer', usersInServer());
 	});
 })
 
@@ -117,6 +126,10 @@ server.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
 
+
+//////////////
+// 전역 함수 //
+//////////////
 function getUserCount() {
 	return usersInServer().length;
 }
